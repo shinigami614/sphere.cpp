bfc6367ac71bffdfce9e1d416746db8a95766163
sam.daniel <billbrat371@gmail.com>
2025-12-05 17:15:29 +0300

    Add files via upload

---
+++ sphere.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+const float p =3.14;
+float volume(float r){
+    return(4.0/3.0)*3*p*r*r*r;
+}
+int main(){
+    float vol ,r;
+    cout<<"Enter your radius"<<endl;
+    cin>>r;
+    vol = volume(r);
+    cout<<"The volume is :"<<vol<<endl;
+    return 0;
+}
